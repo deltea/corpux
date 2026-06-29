771ba1dc78279782d66fdc20197e554cb4714651
deltea <thcheetah777@gmail.com>
2026-06-28 19:47:10 -0700

    somewhat working movement controller

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,17 +1,17 @@
 class_name Player extends CharacterBody3D
 
 const FRICTION = 6.0
+const AIR_FRICTION = 4.5
 const MAX_SPEED = 20.0
 const GROUND_ACCEL = 14.0
 const GROUND_DECEL = 10.0
 const AIR_ACCEL = 14.0
 const DASH_SPEED = 80.0
 const DASH_ACCEL = 100.0
-# const WALL_JUMP_PUSHBACK = 30.0
-# const WALL_JUMP_FORCE = 15.0
-const JUMP_HEIGHT = 1.8
-const DASH_FORCE = 50.0
-const GRAVITY = 25.0
+const WALL_JUMP_PUSHBACK = 40.0
+const WALL_JUMP_FORCE = 15.0
+const JUMP_HEIGHT = 2.0
+const GRAVITY = 28.0
 
 @export var cam_pivot: Camera
 
@@ -33,12 +33,19 @@ func _physics_process(delta: float):
 	dt = delta
 
 	var input = Input.get_vector("left", "right", "forward", "backward")
-	wishdir = (cam_pivot.transform.basis * Vector3(input.x, 0, input.y)).normalized()
+	wishdir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, cam_pivot.rotation.y).normalized()
 
 	check_grounded()
 
 	if is_grounded: ground_move()
 	else: air_move()
+
+	if Input.is_action_just_pressed("jump") and is_on_wall() and not is_grounded:
+		var normal = get_wall_normal()
+		vel -= normal * minf(vel.dot(-normal), 0.0)
+		vel.y = WALL_JUMP_FORCE
+		vel.x = normal.x * WALL_JUMP_PUSHBACK
+		vel.z = normal.z * WALL_JUMP_PUSHBACK
 
 	if Input.is_action_just_pressed("dash") and wishdir != Vector3.ZERO:
 		start_dash()
@@ -47,20 +54,27 @@ func _physics_process(delta: float):
 	move_and_slide()
 
 func ground_move():
-	apply_friction()
-	accelerate(wishdir, wishdir.length_squared() * MAX_SPEED, GROUND_ACCEL)
+	apply_friction(FRICTION)
+	if is_dashing:
+		accelerate(dash_dir, DASH_SPEED, DASH_ACCEL)
+	else:
+		accelerate(wishdir, MAX_SPEED, GROUND_ACCEL)
 	if Input.is_action_just_pressed("jump"):
 		vel.y = sqrt(4 * JUMP_HEIGHT * GRAVITY)
 
 func air_move():
-	accelerate(wishdir, wishdir.length_squared() * MAX_SPEED, AIR_ACCEL)
+	if is_dashing:
+		accelerate(dash_dir, DASH_SPEED, DASH_ACCEL)
+	else:
+		# if wishdir == Vector3.ZERO:
+		apply_friction(AIR_FRICTION)
+		accelerate(wishdir, MAX_SPEED, AIR_ACCEL)
 	vel.y -= GRAVITY * dt
 
 func start_dash():
 	$DashTimer.start()
 	is_dashing = true
 	dash_dir = wishdir
-	accelerate(dash_dir, wishdir.length_squared() * DASH_SPEED, DASH_ACCEL)
 
 func accelerate(dir: Vector3, wishspeed: float, accel: float):
 	var curr_speed = vel.dot(dir)
@@ -72,18 +86,14 @@ func accelerate(dir: Vector3, wishspeed: float, accel: float):
 	vel.x += accel_speed * dir.x
 	vel.z += accel_speed * dir.z
 
-func apply_friction():
+func apply_friction(friction: float):
 	var v = Vector3(vel.x, 0, vel.z)
 	var last_speed = v.length()
-	var control = 0.0
-	var drop = 0.0
 
-	if is_grounded:
-		control = GROUND_DECEL if last_speed < GROUND_DECEL else last_speed
-		drop = control * FRICTION * dt
+	var control = GROUND_DECEL if last_speed < GROUND_DECEL else last_speed
+	var drop = control * friction * dt
 
-	var new_speed = last_speed - drop
-	if new_speed < 0: new_speed = 0
+	var new_speed = maxf(last_speed - drop, 0.0)
 	if last_speed > 0: new_speed /= last_speed
 
 	vel.x *= new_speed
@@ -98,6 +108,6 @@ func check_grounded():
 
 func _on_dash_timer_timeout() -> void:
 	is_dashing = false
-	vel.x = dash_dir.x * 20
-	vel.z = dash_dir.z * 20
+	vel.x = 0
+	vel.z = 0
 	vel.y = 0
