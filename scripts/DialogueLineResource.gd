148d23d12c8c7ce69711ddfcc5bec7e64e281b5e
deltea <thcheetah777@gmail.com>
2026-07-01 19:10:39 -0700

    create resources for dialogue

---
+++ scripts/DialogueLineResource.gd
@@ -0,0 +1,9 @@
+class_name DialogueLineResource extends Resource
+
+## the status of the speaker, can get passed to the speaker
+@export var status: String = "neutral"
+
+## background color of the dialogue box
+@export var color: Color = Color("#0049FF")
+
+@export_multiline var text: String
