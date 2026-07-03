88ce1255ec5b53838561279bf95ce57268103ca9
deltea <thcheetah777@gmail.com>
2026-07-02 17:02:49 -0700

    voice resource

---
+++ scripts/VoiceAlphabetResource.gd
@@ -0,0 +1,4 @@
+class_name VoiceAlphabetResource extends Resource
+
+@export var alphabet: AudioStream
+@export var pitch = 1.0
---
+++ scripts/VoiceResource.gd
@@ -0,0 +1,31 @@
+class_name VoiceResource extends Resource
+
+@export_category("alphabet")
+
+@export var a: VoiceAlphabetResource
+@export var b: VoiceAlphabetResource
+@export var c: VoiceAlphabetResource
+@export var d: VoiceAlphabetResource
+@export var e: VoiceAlphabetResource
+@export var f: VoiceAlphabetResource
+@export var g: VoiceAlphabetResource
+@export var h: VoiceAlphabetResource
+@export var i: VoiceAlphabetResource
+@export var j: VoiceAlphabetResource
+@export var k: VoiceAlphabetResource
+@export var l: VoiceAlphabetResource
+@export var m: VoiceAlphabetResource
+@export var n: VoiceAlphabetResource
+@export var o: VoiceAlphabetResource
+@export var p: VoiceAlphabetResource
+@export var q: VoiceAlphabetResource
+@export var r: VoiceAlphabetResource
+@export var s: VoiceAlphabetResource
+@export var t: VoiceAlphabetResource
+@export var u: VoiceAlphabetResource
+@export var v: VoiceAlphabetResource
+@export var w: VoiceAlphabetResource
+@export var x: VoiceAlphabetResource
+@export var y: VoiceAlphabetResource
+@export var z: VoiceAlphabetResource
+
