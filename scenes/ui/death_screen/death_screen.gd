2b0c57a5551addc571b3ec31b15a96db1a6f0524
deltea <thcheetah777@gmail.com>
2026-07-21 03:02:47 +0800

    make skull flicker faster

--- scenes/ui/death_screen/death_screen.gd
+++ scenes/ui/death_screen/death_screen.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 	tween.tween_property(instructions_label, "visible_ratio", 1.0, 0.5).set_delay(0.2)
 
 	tween = create_tween().set_ignore_time_scale()
-	Tweeny.tween_property_blink(tween, skull, "self_modulate:a", 0.0, 1.0, 1.0)
+	Tweeny.tween_property_blink(tween, skull, "self_modulate:a", 0.0, 1.0, 0.75)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
