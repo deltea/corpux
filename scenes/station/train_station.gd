29daee53ee8d5524dad28139c4062ffcbdc7fef9
deltea <thcheetah777@gmail.com>
2026-08-11 22:12:12 +0800

    add some "sunlight" to the scene

--- scenes/station/train_station.gd
+++ scenes/station/train_station.gd
@@ -1,6 +1,9 @@
 class_name TrainStation extends Node3D
 
 
+const skyhole_scene := preload("res://scenes/environment/skyhole.tscn")
+
+
 const TRAIN_START_X = 500.0
 
 
@@ -9,6 +12,19 @@ const TRAIN_START_X = 500.0
 
 @onready var train_anchor: Node3D = $TrainAnchor
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@onready var combiner: CSGCombiner3D = $CSGCombiner3D
+
+
+func _ready() -> void:
+	var hole_count := 10
+	var hole_sep := (1100.0 / 2 - 48.0) / hole_count
+	for i in range(hole_count):
+		var skyhole := skyhole_scene.instantiate() as CSGBox3D
+		skyhole.position = Vector3(i * hole_sep, 10.0, 12.0)
+		# skyhole.scale.x = 0.5
+		# skyhole.scale.y = 0.5
+		combiner.add_child(skyhole)
 
 
 func _physics_process(dt: float) -> void:
